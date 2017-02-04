87012faf0ca9b4adda65e4b68156bfe8cfad8ccc
Thomas Hobohm <superman3275@gmail.com>
2017-02-03 21:50:05 -0600

    Set up basic canvas.

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  var supremeBackgroundColor = '#fe0000';
+  var canvas = document.getElementById('logo');
+  var context = canvas.getContext('2d');
+
+  context.fillStyle = supremeBackgroundColor;
+  context.fillRect(0, 0, canvas.width, canvas.height);
+});
