15ba9c893a6f75895620c2e4c77eb58183749ec4
Thomas Hobohm <superman3275@gmail.com>
2017-02-03 22:55:57 -0600

    Fixed stuff.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,34 @@
-$(document).ready(function() {
-  var supremeBackgroundColor = '#fe0000';
+supremeBackgroundColor = '#fe0000';
+supremeTextColor = '#ffffff';
+supremeFont = '200px Futura';
+
+function overwrite() {
+  var canvas = document.getElementById('logo');
+  var context = canvas.getContext('2d');
+  context.fillStyle = supremeBackgroundColor;
+  context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function updateText(t) {
   var canvas = document.getElementById('logo');
   var context = canvas.getContext('2d');
 
-  context.fillStyle = supremeBackgroundColor;
-  context.fillRect(0, 0, canvas.width, canvas.height);
+  var x = canvas.width/2;
+  var y = canvas.height/2;
 
+
+  // Erase old text
+  overwrite();
+
+  context.font = supremeFont;
+  context.textBaseline = 'middle';
+  context.textAlign = "center";
+  context.fillStyle = supremeTextColor;
+  context.fillText(t, x, y);
+}
+
+$(document).ready(function() {
+  overwrite();
   $('input').change(function() {
     var supreme_text = $(this).val();
     updateText(supreme_text);
