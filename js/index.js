ee881551997e1cedc10798baf8ce109ee28048e9
Justin Wei <wei.justin.a@gmail.com>
2017-02-03 22:47:01 -0600

    added input

--- js/index.js
+++ js/index.js
@@ -1,27 +1,13 @@
-supremeBackgroundColor = '#fe0000';
-supremeTextColor = '#ffffff';
-supremeFont = '200px Futura';
-
-function updateText(t) {
-  var canvas = document.getElementById('logo');
-  var context = canvas.getContext('2d');
-
-  var x = canvas.width/2;
-  var y = canvas.height/2;
-
-  context.font = supremeFont;
-  context.fillStyle = supremeTextColor;
-  context.textBaseline = 'middle';
-  context.textAlign = "center";
-  context.fillText(t, x, y);
-}
-
 $(document).ready(function() {
+  var supremeBackgroundColor = '#fe0000';
   var canvas = document.getElementById('logo');
   var context = canvas.getContext('2d');
 
   context.fillStyle = supremeBackgroundColor;
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  updateText("Supreme");
+  $('input').change(function() {
+    var supreme_text = $(this).val();
+    updateText(supreme_text);
+  })
 });
