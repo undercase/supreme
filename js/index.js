ee34e1c7a0beba1156284812b07af246f58e6bb3
Justin Wei <wei.justin.a@gmail.com>
2017-02-03 22:52:42 -0600

    minor text changes

--- js/index.js
+++ js/index.js
@@ -6,8 +6,26 @@ $(document).ready(function() {
   context.fillStyle = supremeBackgroundColor;
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  $('input').change(function() {
+  supremeBackgroundColor = '#fe0000';
+  supremeTextColor = '#ffffff';
+  supremeFont = '200px Futura';
+
+  function updateText(t) {
+    var canvas = document.getElementById('logo');
+    var context = canvas.getContext('2d');
+
+    var x = canvas.width/2;
+    var y = canvas.height/2;
+
+    context.font = supremeFont;
+    context.fillStyle = supremeTextColor;
+    context.textBaseline = 'middle';
+    context.textAlign = "center";
+    context.fillText(t, x, y);
+  }
+
+  $('input').keyup(function() {
     var supreme_text = $(this).val();
     updateText(supreme_text);
-  })
+  });
 });
